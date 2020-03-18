2df19164ec56de082a6c66e0c6d8eec5dcc6dac2
Aditya Hajare <aditya43@gmail.com>
2020-03-18 21:40:19 +0530

    Added docker-entrypoint.sh

---
+++ 04-Bind-Mount-Jekyll-Static-Site/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+if [ ! -f Gemfile ]; then
+  echo "NOTE: hmm, I don't see a Gemfile so I don't think there's a jekyll site here"
+  echo "Either you didn't mount a volume, or you mounted it incorrectly."
+  echo "Be sure you're in your jekyll site root and use something like this to launch"
+  echo ""
+  echo "NOTE: To create a new site, you can use the sister image bretfisher/jekyll like:"
+  exit 1
+fi
+
+bundle install
+
+exec "$@"
